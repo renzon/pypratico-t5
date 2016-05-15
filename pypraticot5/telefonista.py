d68176bb4f65691e81059a6c16235dfc8938b88e
renzon <renzon@gmail.com>
2016-05-15 11:51:11 -0300

    Criando mock artesanal

--- pypraticot5/telefonista.py
+++ pypraticot5/telefonista.py
@@ -1,9 +1,16 @@
+from pypraticot5.telefone import Telefone
+
+
 class Telefonista(object):
     def __init__(self):
+        self._telefone = Telefone()
         self._contatos = []
 
     def adicionar(self, contato):
         self._contatos.append(contato)
 
-    def ligar(self):
-        return ['Telefonando de verdade para 2345678. Olá Renzo']
+    def ligar(self, telefone=None):
+        telefone = telefone or self._telefone
+        for nome, numero_telefone in self._contatos:
+            msg = telefone.telefonar(numero_telefone)
+            yield '{msg}. Olá {nome}'.format(msg=msg, nome=nome)
--- tests/telefonista_tests.py
+++ tests/telefonista_tests.py
@@ -1,18 +1,30 @@
 import unittest
 
 from pypraticot5 import telefonista
+from pypraticot5.telefone import Telefone
 
 
-class TelefonistasTests(unittest.TestCase):
+class TelefoneDeMentira(object):
+    def __init__(self):
+        self.numero = None
+
+    def telefonar(self, numero):
+        self.numero = numero
+        return 'Telefonando de mentira para {}'.format(numero)
+
+
+class TelefonistaBasicTests(unittest.TestCase):
     def test_instanciacao_telefonista(self):
         telefonista_obj = telefonista.Telefonista()
         self.assertListEqual([], telefonista_obj._contatos)
+        self.assertIsInstance(telefonista_obj._telefone, Telefone)
 
     def test_adicionar_um_contato(self):
         contato = ('Renzo', '2345678')
         telefonista_obj = telefonista.Telefonista()
         telefonista_obj.adicionar(contato)
         self.assertListEqual([contato], telefonista_obj._contatos)
+
     def test_adicionar_dois_contato(self):
         renzo = ('Renzo', '2345678')
         karen = ('Karen', '8765432')
@@ -22,9 +34,33 @@ class TelefonistasTests(unittest.TestCase):
         self.assertListEqual([renzo, karen], telefonista_obj._contatos)
 
     def test_ligar_para_um_contato(self):
-        contato = ('Renzo', '2345678')
+        renzo = ('Renzo', '2345678')
         telefonista_obj = telefonista.Telefonista()
-        telefonista_obj.adicionar(contato)
-        ligacoes=telefonista_obj.ligar()
+        telefonista_obj.adicionar(renzo)
+        telefone_de_mentira = TelefoneDeMentira()
+        ligacoes = list(telefonista_obj.ligar(telefone_de_mentira))
+        self.assertListEqual(['Telefonando de mentira para 2345678. Olá Renzo'],
+                             ligacoes)
+        self.assertEqual(renzo[1], telefone_de_mentira.numero)
+
+    def test_ligar_para_dois_contato(self):
+        renzo = ('Renzo', '2345678')
+        karen = ('Karen', '8765432')
+        telefonista_obj = telefonista.Telefonista()
+        telefonista_obj.adicionar(renzo)
+        telefonista_obj.adicionar(karen)
+        telefone_de_mentira = TelefoneDeMentira()
+        ligacoes = list(telefonista_obj.ligar(telefone_de_mentira))
+        self.assertListEqual(['Telefonando de mentira para 2345678. Olá Renzo',
+                              'Telefonando de mentira para 8765432. Olá Karen'],
+                             ligacoes)
+
+
+class TelefonistaIntegracaoTests(unittest.TestCase):
+    def test_ligar_para_dois_contato(self):
+        renzo = ('Renzo', '2345678')
+        telefonista_obj = telefonista.Telefonista()
+        telefonista_obj.adicionar(renzo)
+        ligacoes = list(telefonista_obj.ligar())
         self.assertListEqual(['Telefonando de verdade para 2345678. Olá Renzo'],
                              ligacoes)
