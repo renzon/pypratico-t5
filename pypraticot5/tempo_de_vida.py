cbf60ea3e80eae42e60eb69aa20f8dd7b861f7d5
renzon <renzon@gmail.com>
2016-05-22 12:03:23 -0300

    lib de mock para testar código dependente de hora atual

--- pypraticot5/tempo_de_vida.py
+++ pypraticot5/tempo_de_vida.py
@@ -2,8 +2,12 @@ from datetime import datetime
 
 
 def calcular_minutos(nascimento):
-    datahora_atual = datetime.now()
+    datahora_atual = agora()
     time_delta = datahora_atual - nascimento
     segundos = time_delta.total_seconds()
     minutos = int(segundos / 60)
     return minutos
+
+# não mexa nesse código, está aqui para fim de testes
+def agora():
+    return datetime.now()
--- tests/tempo_de_vida_tests.py
+++ tests/tempo_de_vida_tests.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from unittest.case import TestCase
+from unittest.mock import patch
 
 from pypraticot5.tempo_de_vida import calcular_minutos
 
 
 class MinutosDeVidaTests(TestCase):
-    def test_recem_nascido(self):
+    @patch('pypraticot5.tempo_de_vida.agora')
+    def test_recem_nascido(self, agora_mock):
+        agora_mock.return_value = datetime(2016, 5, 22, 11, 48, 2)
         nascimento = datetime(2016, 5, 22, 11, 47, 0)
         minutos_de_vida = calcular_minutos(nascimento)
         self.assertEqual(1, minutos_de_vida)
