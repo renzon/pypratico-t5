4c3c542974669834bf927eaf259f7281d1b0eaad
renzon <renzon@gmail.com>
2016-05-22 10:45:16 -0300

    Comentando solução inocenteˆ

--- tests/github_tests.py
+++ tests/github_tests.py
@@ -7,6 +7,7 @@ from pypraticot5.github import buscar_avatar
 
 class BuscarAvatarBasicoUnitario(TestCase):
     def test_buscar_avatar_de_usuario(self):
+        # Mockando Objetos
         response_mock = Mock()
 
         def json():
@@ -16,9 +17,13 @@ class BuscarAvatarBasicoUnitario(TestCase):
         get_mock = Mock(return_value=response_mock)
         get_real=github.requests.get
         github.requests.get = get_mock
+
+        # Realizando testes
         resultado = buscar_avatar('renzon')
-        self.assertEqual('https://avatars.githubusercontent.com/u/3457115?v=3', resultado)
+        self.assertEqual('https://avatars.githubusercontent.com/u/3457115?v=', resultado)
         get_mock.assert_called_once_with('https://api.github.com/users/renzon')
+
+        # Desfazendo mock
         github.requests.get = get_real
 
 
