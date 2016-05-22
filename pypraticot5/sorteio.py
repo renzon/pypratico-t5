18bbad0661bab19f27bcaf3411857b0f1b68b4a6
renzon <renzon@gmail.com>
2016-05-22 11:27:44 -0300

    Testando lançamento de exceção de forma inocente

--- pypraticot5/sorteio.py
+++ pypraticot5/sorteio.py
@@ -1,2 +1,8 @@
+class SorteioExcecao(Exception):
+    pass
+
+
 def sortear(lista):
+    if len(lista)==0:
+        raise  Exception('Não é possível sortear elemento de lista vazia')
     return lista[0]
--- tests/sorteio_tests.py
+++ tests/sorteio_tests.py
@@ -1,6 +1,6 @@
 from unittest.case import TestCase
 
-from pypraticot5.sorteio import sortear
+from pypraticot5.sorteio import sortear, SorteioExcecao
 
 
 class SortearTests(TestCase):
@@ -9,3 +9,11 @@ class SortearTests(TestCase):
         self.assertEqual(1, resultado)
         resultado = sortear([2])
         self.assertEqual(2, resultado)
+
+    def test_sortear_lista_vazia(self):
+        try:
+            sortear([])
+        except SorteioExcecao:
+            pass
+        else:
+            self.fail('Deveria ter lançado erro Sorteio Exceção')
